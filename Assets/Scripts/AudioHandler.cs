3ee90ae3eb920a5b7f64eadc91e20343992685b2
Bruno Assarisse <bruno@assarisse.com.br>
2015-08-17 01:35:01 -0300

    Bug fix on audio handler.

--- Assets/Scripts/AudioHandler.cs
+++ Assets/Scripts/AudioHandler.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class AudioHandlerHelper : MonoBehaviour {
+	
+	private IDictionary<string, AudioClip> _audioClips = new Dictionary<string, AudioClip>();
 
 	void Start() {
 		//gameObject.AddComponent<AudioSource> ();
@@ -13,7 +15,49 @@ public class AudioHandlerHelper : MonoBehaviour {
 	}
 
 	public void OnLevelWasLoaded(int unused) {
-		AudioHandler.Cleanup ();
+		Cleanup ();
+	}
+	
+	public bool Load(string audioName) {
+		
+		if (_audioClips.ContainsKey (audioName))
+			return true;
+		
+		var audioClip = Resources.Load ("Audio/" + audioName) as AudioClip;
+		if (audioClip == null)
+			return false;
+		
+		_audioClips.Add (audioName, audioClip);
+		
+		return true;
+		
+	}
+	
+	public bool Play(string audioName, float volume = 1.0f) {
+		return Play (audioName, Vector3.zero, volume);
+	}
+	
+	public bool Play(string audioName, Vector3 point, float volume = 1.0f) {
+		
+		if (!_audioClips.ContainsKey (audioName)) {
+			if (!Load(audioName))
+				return false;
+		}
+		
+		var audioClip = _audioClips[audioName];
+		if (audioClip == null)
+			return false;
+		
+		AudioSource.PlayClipAtPoint (audioClip, point, volume);
+		
+		return true;
+	}
+	
+	public void Cleanup() {
+		foreach (var item in _audioClips) {
+			Resources.UnloadAsset (item.Value);
+		}
+		_audioClips.Clear ();
 	}
 }
 
@@ -24,43 +68,20 @@ static internal class AudioHandler {
 	static private AudioHandlerHelper _audioHandlerHelper = ( new GameObject("AudioHandlerHelper") ).AddComponent<AudioHandlerHelper>();
 	#pragma warning restore 0414
 
-	static private IDictionary<string, AudioClip> _audioClips = new Dictionary<string, AudioClip>();
-
 	static public bool Load(string audioName) {
-
-		if (_audioClips.ContainsKey ("audioName"))
-			return true;
-
-		var audioClip = Resources.Load ("Audio/" + audioName) as AudioClip;
-		if (audioClip == null)
-			return false;
-
-		_audioClips.Add (audioName, audioClip);
-
-		return true;
-		
+		return _audioHandlerHelper.Load (audioName);
 	}
 	
 	static public bool Play(string audioName, float volume = 1.0f) {
-		return Play (audioName, Vector3.zero, volume);
+		return _audioHandlerHelper.Play (audioName, volume);
 	}
 	
 	static public bool Play(string audioName, Vector3 point, float volume = 1.0f) {
-		
-		var audioClip = _audioClips[audioName];
-		if (audioClip == null)
-			return false;
-
-		AudioSource.PlayClipAtPoint (audioClip, point, volume);
-
-		return true;
+		return _audioHandlerHelper.Play (audioName, point, volume);
 	}
 	
 	static public void Cleanup() {
-		foreach (var item in _audioClips) {
-			Resources.UnloadAsset (item.Value);
-		}
-		_audioClips.Clear ();
+		_audioHandlerHelper.Cleanup ();
 	}
 
 }
