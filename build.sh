2d06d41304e72efe86a00e21c5a8ab19e91465f9
Bruno Assarisse <bruno@assarisse.com.br>
2015-08-24 22:21:34 -0300

    Changing build script to leave OSX for last.

--- build.sh
+++ build.sh
@@ -13,16 +13,16 @@ mkdir $FINAL_PATH
 cd $FINAL_PATH
 
 mkdir linux
-mkdir osx
 mkdir web
 mkdir win32
+mkdir osx
 
 cd $CURRENT_PATH
 
 $UNITY_PATH -quit -batchmode -buildLinux32Player $FINAL_PATH/linux/$GAME_NAME.x86
-$UNITY_PATH -quit -batchmode -buildOSXPlayer $FINAL_PATH/osx/$GAME_NAME.app
 $UNITY_PATH -quit -batchmode -buildWebPlayer $FINAL_PATH/web/$GAME_NAME
 $UNITY_PATH -quit -batchmode -buildWindowsPlayer $FINAL_PATH/win32/$GAME_NAME.exe
+$UNITY_PATH -quit -batchmode -buildOSXPlayer $FINAL_PATH/osx/$GAME_NAME.app
 
 cd $FINAL_PATH
 
@@ -30,16 +30,16 @@ rm -rf win32/*.pdb
 
 cd $FINAL_PATH/linux
 zip -r -X -q ../$GAME_NAME-linux *
-cd $FINAL_PATH/osx
-zip -r -X -q ../$GAME_NAME-osx *
 cd $FINAL_PATH/win32
 zip -r -X -q ../$GAME_NAME-win32 *
+cd $FINAL_PATH/osx
+zip -r -X -q ../$GAME_NAME-osx *
 
 cd $FINAL_PATH
 
 cp -f web/$GAME_NAME/*.unity3d ./
 
 rm -rf linux
-rm -rf osx
 rm -rf web
 rm -rf win32
+rm -rf osx
