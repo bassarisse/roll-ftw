c8c8aed8254aef13f3a041d536c29011aecbf38c
Bruno Assarisse <bruno@assarisse.com.br>
2015-09-07 15:56:17 -0300

    Skiping GameJolt login on Android.

--- Assets/Scripts/GameJoltLogin.cs
+++ Assets/Scripts/GameJoltLogin.cs
@@ -6,7 +6,7 @@ public class GameJoltLogin : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-#if UNITY_WEBPLAYER || UNITY_WEBGL
+#if UNITY_WEBPLAYER || UNITY_WEBGL || UNITY_ANDROID
 		Next();
 #else
 		var isSignedIn = GameJolt.API.Manager.Instance.CurrentUser != null;
